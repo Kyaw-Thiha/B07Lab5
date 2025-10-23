6db1a5c9f6f423a8e18e88ae1f83ad7dc90e3166
najjalowe <najjaalowe@gmail.com>
2025-10-23 16:10:46 -0400

    feat(circle): add Circle implementation (circumference + area)

---
+++ src/main/lab5/Circle.java
@@ -0,0 +1,19 @@
+package Lab5;
+
+public class Circle{
+    Point center;
+    double radius;
+
+    public Circle(Point c, double r){
+        this.center = c;
+        this.radius = r;
+    }
+
+    public double circumference(){
+        return 2*(Math.PI)*this.radius;
+    }
+
+    public double area(){
+        return (Math.PI)*Math.pow(r,2);
+    }
+}
